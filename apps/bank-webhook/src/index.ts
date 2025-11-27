49ac2de0d4bfb4088a0530fade5a6249dda049b8
Abhinav2005-hub <abhinavsaini995@gmail.com>
2025-11-27 23:30:06 +0530

    add transaction

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -10,27 +10,35 @@ app.post("/hdfcWebhook", async (req,res) => {
         userId: req.body.userIdentifier,
         amount: req.body.amount
     };
-    await db.balance.update({
-        where: {
-            userId: paymentInformation.userId
-        },
-        data: {
-            amount: {
-                increment: paymentInformation.amount
-            }
-        }
-    });
 
-    await db.onRampTransition.update({
-        where: {
-            token: paymentInformation.token,
-        },
-        data: {
-            status: "Success"
-        }
-    })
-    res.status(200).json({
-        message: "captured"
-    })
-    // Update balance in db
+    try {
+        await db.$transaction([
+            db.balance.updateMany({
+                where: {
+                    userId: Number(paymentInformation.userId)
+                },
+                data: {
+                    amount: {
+                        increment: Number(paymentInformation.amount)
+                    }
+                }
+            }),
+            await db.onRampTransacition.updateMany({
+                where: {
+                    token: paymentInformation.token,
+                },
+                data: {
+                    status: "Success"
+                }
+            })
+        ])
+        res.json({
+            message: "captured"
+        })
+    } catch (e){
+        console.error(e);
+        res.status(411).json({
+            message:  "Error while processing webhook"
+        })
+    }
 })
