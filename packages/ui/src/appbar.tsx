07557b4c72af8c2ada086edc5868f28067331f6a
Abhinav2005-hub <abhinavsaini995@gmail.com>
2025-11-29 22:37:12 +0530

    update file

--- packages/ui/src/appbar.tsx
+++ packages/ui/src/appbar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "./button";
 
 interface AppbarProps {
@@ -8,11 +10,12 @@ interface AppbarProps {
 
 export const Appbar = ({ user, onSignin, onSignout }: AppbarProps) => {
   return (
-    <div className="flex justify-between border-b px-4">
+    <div className="flex justify-between border-b px-4 py-2">
       <div className="text-lg flex flex-col justify-center">
         PayTM
       </div>
-      <div className="flex flex-col justify-center pt-2">
+
+      <div className="flex flex-col justify-center">
         <Button onClick={user ? onSignout : onSignin}>
           {user ? "Logout" : "Login"}
         </Button>
