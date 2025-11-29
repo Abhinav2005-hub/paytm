57dea7de34ea4f38b8314b5f1915fd95c09de326
Abhinav2005-hub <abhinavsaini995@gmail.com>
2025-11-29 22:33:41 +0530

    solve error

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -2,43 +2,37 @@ import express from "express";
 import db from "@repo/db";
 
 const app = express();
+app.use(express.json());
 
-app.post("/hdfcWebhook", async (req,res) => {
-    //TODO: Add zod validation here?
-    const paymentInformation = {
-        token: req.body.token,
-        userId: req.body.userIdentifier,
-        amount: req.body.amount
-    };
+app.post("/hdfcbewbhooook", async (req, res) => {
+  const paymentInformation = {
+    token: req.body.token,
+    userId: req.body.userIdentifier,
+    amount: req.body.amount,
+  };
 
-    try {
-        await db.$transaction([
-            db.balance.updateMany({
-                where: {
-                    userId: Number(paymentInformation.userId)
-                },
-                data: {
-                    amount: {
-                        increment: Number(paymentInformation.amount)
-                    }
-                }
-            }),
-            await db.onRampTransacition.updateMany({
-                where: {
-                    token: paymentInformation.token,
-                },
-                data: {
-                    status: "Success"
-                }
-            })
-        ])
-        res.json({
-            message: "captured"
-        })
-    } catch (e){
-        console.error(e);
-        res.status(411).json({
-            message:  "Error while processing webhook"
-        })
-    }
-})
+  try {
+    await db.$transaction([
+      db.balance.updateMany({
+        where: { userId: Number(paymentInformation.userId) },
+        data: {
+          amount: { increment: Number(paymentInformation.amount) },
+        },
+      }),
+
+      db.onRampTransaction.updateMany({
+        where: { token: paymentInformation.token },
+        data: {
+          status: "Success",
+        },
+      }),
+    ]);
+
+    res.json({ message: "Captured" });
+  } catch (err) {
+    console.log(err);
+    res.status(411).json({ message: "Error while processing webhook" });
+  }
+});
+
+app.listen(3003);
