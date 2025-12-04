911d709adfc24c4e13295a9c033dddbdb9016ca5
Abhinav2005-hub <abhinavsaini995@gmail.com>
2025-12-04 21:02:23 +0530

    finish onramp

--- apps/user-app/components/AddMoneyCard.tsx
+++ apps/user-app/components/AddMoneyCard.tsx
@@ -5,6 +5,7 @@ import { Center } from "@repo/ui/Center";
 import { Select } from "@repo/ui/Select";
 import { useState } from "react";
 import { TextInput } from "@repo/ui/TextInput";
+import { createOnRampTransaction } from "../app/lib/actions/createOnRamptxn";
 
 const SUPPORTED_BANK = [{
     name: "HDFC Bank",
@@ -16,24 +17,29 @@ const SUPPORTED_BANK = [{
 
 export const AddMoney = () => {
     const [redirectUrl, setRedirectUrl] = useState(SUPPORTED_BANK[0]?.redirectUrl);
+    const [amount, setAmount] = useState(0);
+    const [provider, setProvider] = useState(SUPPORTED_BANK[0]?.name || "");
+
     return <Card title = "Add Money" className="max-w-md w-full">
         <div className="w-full">
-            <TextInput label={"Amount"} placeholder={"Amount"} onChange={() => {
-
+            <TextInput label={"Amount"} placeholder={"Amount"} onChange={(value) => {
+                setAmount(value)
             }}/>
             <div className="py-4 text-left">
                 Bank
             </div>
             <Select onSelect={(value) => {
                 setRedirectUrl(SUPPORTED_BANK.find(x => x.name === value)?.redirectUrl || "")
+                setProvider(SUPPORTED_BANK.find(x => x.name === value)?.name || "")
             }} options = {SUPPORTED_BANK.map(x => ({
                 key: x.name,
                 value: x.name
             }))} />
             <div className="pt-4 px-4 flex justify-center">
-               <Button className="px-6 py-2 bg-black text-white rounded"
-               onClick={() => window.location.href = redirectUrl || ""}
-               >
+               <Button className="px-6 py-2 bg-black text-white rounded"onClick={async() => {
+                   await createOnRampTransaction(amount * 100, provider)
+                   window.location.href = redirectUrl || ""
+                }}>
                 Add Money
                </Button>
             </div>
